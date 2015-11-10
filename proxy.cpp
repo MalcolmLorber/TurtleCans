1512663b91a21d9236b4f35cffaf3dc5bd5da359
SamuelWenninger <wennis@rpi.edu>
2015-11-10 11:38:40 -0500

    Main spec

---
+++ proxy.cpp
@@ -0,0 +1,28 @@
+/*******************************************************************************
+Name: Samuel Wenninger, Brandon Drumheller, and Malcolm Lorber
+Date Created: 11-10-2015
+Filename: proxy.cpp
+Description: Proxy program that only transports data to and from the bank
+             program using the Boost libarary.
+*******************************************************************************/
+
+#include <iostream>
+
+
+
+
+
+
+
+
+
+/*******************************************************************************
+ @DESC: Call appropriate function(s) to initiate communication between the ATM
+        and the bank. 
+ @ARGS: port to listen on, port to connect to the bank
+ @RTN: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+*******************************************************************************/
+int main (int argc, char* argv[]) {
+    
+    return EXIT_SUCCESS;
+}
