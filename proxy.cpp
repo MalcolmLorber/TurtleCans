91c868af5830502977e7f5d38ea0a4c0d65ef8bc
SamuelWenninger <wennis@rpi.edu>
2015-11-10 11:58:17 -0500

    Basic proxy spec

--- proxy.cpp
+++ proxy.cpp
@@ -3,12 +3,30 @@ Name: Samuel Wenninger, Brandon Drumheller, and Malcolm Lorber
 Date Created: 11-10-2015
 Filename: proxy.cpp
 Description: Proxy program that only transports data to and from the bank
-             program using the Boost libarary.
+             program using the Boost libarary. Compile using the following:
+             "g++ -std=c++11 proxy.cpp -lboost_system"
 *******************************************************************************/
 
 #include <iostream>
+#include <boost/asio.hpp>
 
 
+
+/*******************************************************************************
+ @DESC: Listen on ATM port and connect to the bank port using the boost:asio
+        library. Multiple connections must be able to be handled at the same
+        time (multiple ATMs connected to the bank at any given time).
+ @ARGS: port to listen on, port to connect to the bank
+ @RTN:  true on success, false on failure
+*******************************************************************************/
+bool Connect(const int & ATMPort, const int & BankPort) {
+
+
+    //If failure, return false
+    
+
+    return true;
+}
 
 
 
@@ -20,9 +38,11 @@ Description: Proxy program that only transports data to and from the bank
  @DESC: Call appropriate function(s) to initiate communication between the ATM
         and the bank. 
  @ARGS: port to listen on, port to connect to the bank
- @RTN: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ @RTN:  EXIT_SUCCESS on success, EXIT_FAILURE on failure
 *******************************************************************************/
 int main (int argc, char* argv[]) {
+
+    //If Connect() returns false, return EXIT_FAILURE
     
     return EXIT_SUCCESS;
 }
