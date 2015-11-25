66be056622e54342935ca5833fe1fadac146a45a
SamuelWenninger <wennis@rpi.edu>
2015-11-25 10:33:36 -0500

    Updated comments

--- proxy.cpp
+++ proxy.cpp
@@ -9,13 +9,18 @@ Description: Proxy program that only transports data to and from the bank
 
 #include <iostream>
 #include <string>
-#include <memory>
-#include <utility>
+//#include <memory>
+//#include <utility>
 #include <boost/asio.hpp>
-//#include <boost/thread/thread.hpp>
 
 using boost::asio::ip::tcp;
 
+/*******************************************************************************
+ @DESC: The Session class is responsible for reading from the ATM socket and
+        to the Bank socket.
+ @ARGS: N/A
+ @RTN:  N/A
+*******************************************************************************/
 class Session : public std::enable_shared_from_this<Session> {
     public:
         Session(tcp::socket Socket1, tcp::socket Socket2) 
@@ -25,6 +30,7 @@ class Session : public std::enable_shared_from_this<Session> {
             DoRead();
         }
     private:
+        //Read from the listen socket (ATM socket)
         void DoRead() {
             auto Self(shared_from_this());    
             socket_.async_read_some(boost::asio::buffer(data_, max_length),
@@ -34,28 +40,31 @@ class Session : public std::enable_shared_from_this<Session> {
                 }
             });
         }
-
+        //Write to the bank socket 
         void DoWrite(std::size_t Length) {
             auto Self(shared_from_this());
-            boost::asio::async_write(bank_socket_, boost::asio::buffer(data_, Length),
-            [this, Self](boost::system::error_code EC, std::size_t){
+            boost::asio::async_write(bank_socket_, 
+                                            boost::asio::buffer(data_, Length),
+                        [this, Self](boost::system::error_code EC, std::size_t){
                 if (!EC) {
                     DoRead();
                 }
             });
         }
+        //Listen socket (ATM socket)
         tcp::socket socket_;
         tcp::socket bank_socket_;
+        //Max length of messages passed through the proxy
         enum {max_length = 1024};
+        //Array to hold the incoming message
         char data_[max_length];
 };
 
 /*******************************************************************************
- @DESC: Listen on ATM port and connect to the bank port using the boost:asio
-        library. Multiple connections must be able to be handled at the same
-        time (multiple ATMs connected to the bank at any given time).
- @ARGS: io_service, port to listen on, port to connect to the bank
- @RTN:  true on success, false on failure
+ @DESC: The Server class is responsible for initializing the connection to both
+        ATM and Bank ports and start a Session instance.
+ @ARGS: N/A
+ @RTN:  N/A
 *******************************************************************************/
 class Server {
     public:
@@ -70,21 +79,28 @@ class Server {
               }
     private:
         void DoAccept() {
-            //Accept connection and start a session by calling the Session
-            //constructor
-            bank_acceptor_.async_accept(bank_socket_, [this](boost::system::error_code EC1) {
+            //Accept a new connection to the bank socket
+            bank_acceptor_.async_accept(bank_socket_,
+                                         [this](boost::system::error_code EC1) {
                 if (!EC1) {
-                    listen_acceptor_.async_accept(listen_socket_, [this](boost::system::error_code EC) {
+                    //Accept a new connection to the listen socket (ATM socket)
+                    listen_acceptor_.async_accept(listen_socket_, 
+                                          [this](boost::system::error_code EC) {
                         if (!EC) {
-                        std::make_shared<Session>(std::move(listen_socket_), std::move(bank_socket_)) -> Start();
+                        //Accept connection and start a session by calling the
+                        //Session constructor
+                        std::make_shared<Session>(std::move(listen_socket_), 
+                                            std::move(bank_socket_)) -> Start();
                         }
                         DoAccept();
                     });
                 }
             });
         }
+        //ATM acceptor and socket
         tcp::acceptor listen_acceptor_;
         tcp::socket listen_socket_;
+        //Bank acceptor and socket
         tcp::acceptor bank_acceptor_;
         tcp::socket bank_socket_;
 };
@@ -99,7 +115,7 @@ class Server {
 int main (int argc, char* argv[]) {
     try {
         if (argc != 3) {
-            std::cout << "Incorrect number of arguments. Proper usage ./a.out"
+            std::cerr << "Incorrect number of arguments. Proper usage ./a.out"
                             " <port-to-listen-on> <bank-port>" << std::endl;
             return EXIT_FAILURE;
         }
@@ -107,6 +123,7 @@ int main (int argc, char* argv[]) {
         int ListenPort = std::stoi(argv[1]);
         int BankPort = std::stoi(argv[2]);
         Server S(IOService, ListenPort, BankPort);
+        //Throws an exception if something goes wrong
         IOService.run();
     }
     catch (std::exception & e) {
