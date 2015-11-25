f8d425a692a822427fe985df2538460f44f96821
SamuelWenninger <wennis@rpi.edu>
2015-11-25 11:32:10 -0500

    Basic implementation

---
+++ bank.cpp
@@ -0,0 +1,117 @@
+/*******************************************************************************
+Name: Samuel Wenninger, Brandon Drumheller, and Malcolm Lorber
+Date Created: 11-25-2015
+Filename: bank.cpp
+Description: Bank server that services requests from the ATM 
+             using the Boost library. Compile using the following:
+             "g++ -std=c++11 proxy.cpp -lboost_system"
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+//#include <memory>
+//#include <utility>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+/*******************************************************************************
+ @DESC: The Session class is responsible for reading from the ATM socket and
+        to the Bank socket.
+ @ARGS: N/A
+ @RTN:  N/A
+*******************************************************************************/
+class Session : public std::enable_shared_from_this<Session> {
+    public:
+        Session(tcp::socket Socket1) 
+            : bank_socket_(std::move(Socket1)) {}
+        void Start() {
+            DoRead();
+        }
+    private:
+        //Read from the listen socket (ATM socket)
+        void DoRead() {
+            auto Self(shared_from_this());    
+            bank_socket_.async_read_some(boost::asio::buffer(data_, max_length),
+            [this, Self](boost::system::error_code EC, std::size_t Length) {
+                if (!EC) {
+                    DoWrite(Length);
+                }
+            });
+        }
+        //Write to the bank socket 
+        void DoWrite(std::size_t Length) {
+            auto Self(shared_from_this());
+            boost::asio::async_write(bank_socket_, 
+                                            boost::asio::buffer(data_, Length),
+                        [this, Self](boost::system::error_code EC, std::size_t){
+                if (!EC) {
+                    DoRead();
+                }
+            });
+        }
+        tcp::socket bank_socket_;
+        //Max length of messages passed through the proxy
+        enum {max_length = 1024};
+        //Array to hold the incoming message
+        char data_[max_length];
+};
+
+/*******************************************************************************
+ @DESC: The Server class is responsible for initializing the connection to both
+        ATM and Bank ports and start a Session instance.
+ @ARGS: N/A
+ @RTN:  N/A
+*******************************************************************************/
+class Server {
+    public:
+        Server(boost::asio::io_service & IOService, int BankPort)
+            //Call acceptor constructor with the bank's port as the endopoint 
+            //and assign result to acceptor_
+            : bank_acceptor_(IOService, tcp::endpoint(tcp::v4(), BankPort)),
+              bank_socket_(IOService) {
+                DoAccept();
+              }
+    private:
+        void DoAccept() {
+            //Accept a new connection to the bank socket
+            bank_acceptor_.async_accept(bank_socket_,
+                                         [this](boost::system::error_code EC) {
+                if (!EC) {
+                        //Accept connection and start a session by calling the
+                        //Session constructor
+                        std::make_shared<Session>(std::move(bank_socket_)) -> Start();
+                        }
+                        DoAccept();
+                    });
+        }
+        //Bank acceptor and socket
+        tcp::acceptor bank_acceptor_;
+        tcp::socket bank_socket_;
+};
+
+
+/*******************************************************************************
+ @DESC: Call appropriate function(s) to initiate communication between the ATM
+        and the bank. 
+ @ARGS: port to listen on, port to connect to the bank
+ @RTN:  EXIT_SUCCESS on success, EXIT_FAILURE on failure
+*******************************************************************************/
+int main (int argc, char* argv[]) {
+    try {
+        if (argc != 2) {
+            std::cerr << "Incorrect number of arguments. Proper usage ./a.out"
+                            " <bank-port>" << std::endl;
+            return EXIT_FAILURE;
+        }
+        boost::asio::io_service IOService;
+        int BankPort = std::stoi(argv[1]);
+        Server S(IOService, BankPort);
+        //Throws an exception if something goes wrong
+        IOService.run();
+    }
+    catch (std::exception & e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
