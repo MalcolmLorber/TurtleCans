3491f6ec597347f00a2d189b6173a52947f0037b
SamuelWenninger <wennis@rpi.edu>
2015-11-27 18:50:57 -0500

    Working proxy

--- proxy.cpp
+++ proxy.cpp
@@ -1,133 +1,293 @@
-/*******************************************************************************
-Name: Samuel Wenninger, Brandon Drumheller, and Malcolm Lorber
-Date Created: 11-10-2015
-Filename: proxy.cpp
-Description: Proxy program that only transports data to and from the bank
-             program using the Boost library. Compile using the following:
-             "g++ -std=c++11 proxy.cpp -lboost_system"
-*******************************************************************************/
+//
+// tcpproxy_server.cpp
+// ~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2007 Arash Partow (http://www.partow.net)
+// URL: http://www.partow.net/programming/tcpproxy/index.html
+//
+// Distributed under the Boost Software License, Version 1.0.
+//
+//
+// Description
+// ~~~~~~~~~~~
+// The objective of the TCP proxy server is to act as an intermediary
+// in order to 'forward' TCP based connections from external clients
+// onto a singular remote server.
+// The communication flow in the direction from the client to the proxy
+// to the server is called the upstream flow, and the communication flow
+// in the direction from the server to the proxy to the client is called
+// the downstream flow. Furthermore the up and down stream connections
+// are consolidated into a single concept known as a bridge.
+// In the event either the downstream or upstream end points disconnect,
+// the proxy server will proceed to disconnect the other end point
+// and eventually destroy the associated bridge.
+//
+// The following is a flow and structural diagram depicting the
+// various elements (proxy, server and client) and how they connect
+// and interact with each other.
+//
+//                                    ---> upstream --->           +---------------+
+//                                                     +---->------>               |
+//                               +-----------+         |           | Remote Server |
+//                     +--------->          [x]--->----+  +---<---[x]              |
+//                     |         | TCP Proxy |            |        +---------------+
+// +-----------+       |  +--<--[x] Server   <-----<------+
+// |          [x]--->--+  |      +-----------+
+// |  Client   |          |
+// |           <-----<----+
+// +-----------+
+//                <--- downstream <---
+//
+//
 
+
+#include <cstdlib>
+#include <cstddef>
 #include <iostream>
 #include <string>
-//#include <memory>
-//#include <utility>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/bind.hpp>
 #include <boost/asio.hpp>
-
-using boost::asio::ip::tcp;
-
-/*******************************************************************************
- @DESC: The Session class is responsible for reading from the ATM socket and
-        to the Bank socket.
- @ARGS: N/A
- @RTN:  N/A
-*******************************************************************************/
-class Session : public std::enable_shared_from_this<Session> {
-    public:
-        Session(tcp::socket Socket1, tcp::socket Socket2) 
-            : socket_(std::move(Socket1)),
-              bank_socket_(std::move(Socket2)) {}
-        void Start() {
-            DoRead();
-        }
-    private:
-        //Read from the listen socket (ATM socket)
-        void DoRead() {
-            auto Self(shared_from_this());    
-            socket_.async_read_some(boost::asio::buffer(data_, max_length),
-            [this, Self](boost::system::error_code EC, std::size_t Length) {
-                if (!EC) {
-                    DoWrite(Length);
-                }
-            });
-        }
-        //Write to the bank socket 
-        void DoWrite(std::size_t Length) {
-            auto Self(shared_from_this());
-            boost::asio::async_write(bank_socket_, 
-                                            boost::asio::buffer(data_, Length),
-                        [this, Self](boost::system::error_code EC, std::size_t){
-                if (!EC) {
-                    DoRead();
-                }
-            });
-        }
-        //Listen socket (ATM socket)
-        tcp::socket socket_;
-        tcp::socket bank_socket_;
-        //Max length of messages passed through the proxy
-        enum {max_length = 1024};
-        //Array to hold the incoming message
-        char data_[max_length];
-};
-
-/*******************************************************************************
- @DESC: The Server class is responsible for initializing the connection to both
-        ATM and Bank ports and start a Session instance.
- @ARGS: N/A
- @RTN:  N/A
-*******************************************************************************/
-class Server {
-    public:
-        Server(boost::asio::io_service & IOService, int ListenPort, int BankPort)
-            //Call acceptor constructor with the bank's port as the endopoint 
-            //and assign result to acceptor_
-            : listen_acceptor_(IOService, tcp::endpoint(tcp::v4(), ListenPort)),
-              bank_acceptor_(IOService, tcp::endpoint(tcp::v4(), BankPort)),
-              listen_socket_(IOService),
-              bank_socket_(IOService) {
-                DoAccept();
-              }
-    private:
-        void DoAccept() {
-            //Accept a new connection to the bank socket
-            bank_acceptor_.async_accept(bank_socket_,
-                                         [this](boost::system::error_code EC1) {
-                if (!EC1) {
-                    //Accept a new connection to the listen socket (ATM socket)
-                    listen_acceptor_.async_accept(listen_socket_, 
-                                          [this](boost::system::error_code EC) {
-                        if (!EC) {
-                        //Accept connection and start a session by calling the
-                        //Session constructor
-                        std::make_shared<Session>(std::move(listen_socket_), 
-                                            std::move(bank_socket_)) -> Start();
-                        }
-                        DoAccept();
-                    });
-                }
-            });
-        }
-        //ATM acceptor and socket
-        tcp::acceptor listen_acceptor_;
-        tcp::socket listen_socket_;
-        //Bank acceptor and socket
-        tcp::acceptor bank_acceptor_;
-        tcp::socket bank_socket_;
-};
+#include <boost/thread/mutex.hpp>
 
 
-/*******************************************************************************
- @DESC: Call appropriate function(s) to initiate communication between the ATM
-        and the bank. 
- @ARGS: port to listen on, port to connect to the bank
- @RTN:  EXIT_SUCCESS on success, EXIT_FAILURE on failure
-*******************************************************************************/
-int main (int argc, char* argv[]) {
-    try {
-        if (argc != 3) {
-            std::cerr << "Incorrect number of arguments. Proper usage ./a.out"
-                            " <port-to-listen-on> <bank-port>" << std::endl;
-            return EXIT_FAILURE;
-        }
-        boost::asio::io_service IOService;
-        int ListenPort = std::stoi(argv[1]);
-        int BankPort = std::stoi(argv[2]);
-        Server S(IOService, ListenPort, BankPort);
-        //Throws an exception if something goes wrong
-        IOService.run();
-    }
-    catch (std::exception & e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    return EXIT_SUCCESS;
+namespace tcp_proxy
+{
+   namespace ip = boost::asio::ip;
+
+   class bridge : public boost::enable_shared_from_this<bridge>
+   {
+   public:
+
+      typedef ip::tcp::socket socket_type;
+      typedef boost::shared_ptr<bridge> ptr_type;
+
+      bridge(boost::asio::io_service& ios)
+      : downstream_socket_(ios),
+        upstream_socket_(ios)
+      {}
+
+      socket_type& downstream_socket()
+      {
+         return downstream_socket_;
+      }
+
+      socket_type& upstream_socket()
+      {
+         return upstream_socket_;
+      }
+
+      void start(const std::string& upstream_host, unsigned short upstream_port)
+      {
+         upstream_socket_.async_connect(
+              ip::tcp::endpoint(
+                   boost::asio::ip::address::from_string(upstream_host),
+                   upstream_port),
+               boost::bind(&bridge::handle_upstream_connect,
+                    shared_from_this(),
+                    boost::asio::placeholders::error));
+      }
+
+      void handle_upstream_connect(const boost::system::error_code& error)
+      {
+         if (!error)
+         {
+            upstream_socket_.async_read_some(
+                 boost::asio::buffer(upstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_upstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+
+            downstream_socket_.async_read_some(
+                 boost::asio::buffer(downstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_downstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+         }
+         else
+            close();
+      }
+
+   private:
+
+      void handle_downstream_write(const boost::system::error_code& error)
+      {
+         if (!error)
+         {
+            upstream_socket_.async_read_some(
+                 boost::asio::buffer(upstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_upstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+         }
+         else
+            close();
+      }
+
+      void handle_downstream_read(const boost::system::error_code& error,
+                                  const size_t& bytes_transferred)
+      {
+         if (!error)
+         {
+            async_write(upstream_socket_,
+                  boost::asio::buffer(downstream_data_,bytes_transferred),
+                  boost::bind(&bridge::handle_upstream_write,
+                        shared_from_this(),
+                        boost::asio::placeholders::error));
+         }
+         else
+            close();
+      }
+
+      void handle_upstream_write(const boost::system::error_code& error)
+      {
+         if (!error)
+         {
+            downstream_socket_.async_read_some(
+                 boost::asio::buffer(downstream_data_,max_data_length),
+                 boost::bind(&bridge::handle_downstream_read,
+                      shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred));
+         }
+         else
+            close();
+      }
+
+      void handle_upstream_read(const boost::system::error_code& error,
+                                const size_t& bytes_transferred)
+      {
+         if (!error)
+         {
+            async_write(downstream_socket_,
+                 boost::asio::buffer(upstream_data_,bytes_transferred),
+                 boost::bind(&bridge::handle_downstream_write,
+                      shared_from_this(),
+                      boost::asio::placeholders::error));
+         }
+         else
+            close();
+      }
+
+      void close()
+      {
+         boost::mutex::scoped_lock lock(mutex_);
+         if (downstream_socket_.is_open())
+            downstream_socket_.close();
+         if (upstream_socket_.is_open())
+            upstream_socket_.close();
+      }
+
+      socket_type downstream_socket_;
+      socket_type upstream_socket_;
+
+      enum { max_data_length = 8192 }; //8KB
+      unsigned char downstream_data_[max_data_length];
+      unsigned char upstream_data_[max_data_length];
+
+      boost::mutex mutex_;
+
+   public:
+
+      class acceptor
+      {
+      public:
+
+         acceptor(boost::asio::io_service& io_service,
+                  const std::string& local_host, unsigned short local_port,
+                  const std::string& upstream_host, unsigned short upstream_port)
+         : io_service_(io_service),
+           localhost_address(boost::asio::ip::address_v4::from_string(local_host)),
+           acceptor_(io_service_,ip::tcp::endpoint(localhost_address,local_port)),
+           upstream_port_(upstream_port),
+           upstream_host_(upstream_host)
+         {}
+
+         bool accept_connections()
+         {
+            try
+            {
+               session_ = boost::shared_ptr<bridge>(new bridge(io_service_));
+               acceptor_.async_accept(session_->downstream_socket(),
+                    boost::bind(&acceptor::handle_accept,
+                         this,
+                         boost::asio::placeholders::error));
+            }
+            catch(std::exception& e)
+            {
+               std::cerr << "acceptor exception: " << e.what() << std::endl;
+               return false;
+            }
+            return true;
+         }
+
+      private:
+
+         void handle_accept(const boost::system::error_code& error)
+         {
+            if (!error)
+            {
+               session_->start(upstream_host_,upstream_port_);
+               if (!accept_connections())
+               {
+                  std::cerr << "Failure during call to accept." << std::endl;
+               }
+            }
+            else
+            {
+               std::cerr << "Error: " << error.message() << std::endl;
+            }
+         }
+
+         boost::asio::io_service& io_service_;
+         ip::address_v4 localhost_address;
+         ip::tcp::acceptor acceptor_;
+         ptr_type session_;
+         unsigned short upstream_port_;
+         std::string upstream_host_;
+      };
+
+   };
 }
+
+int main(int argc, char* argv[])
+{
+   if (argc != 3)
+   {
+      std::cerr << "usage: ./a.out <atm port> <bank port>" << std::endl;
+      return 1;
+   }
+
+   const unsigned short local_port   = static_cast<unsigned short>(::atoi(argv[1]));
+   const unsigned short forward_port = static_cast<unsigned short>(::atoi(argv[2]));
+   const std::string local_host      = "127.0.0.1";
+   const std::string forward_host    = "127.0.0.1";
+
+   boost::asio::io_service ios;
+
+   try
+   {
+      tcp_proxy::bridge::acceptor acceptor(ios,
+                                           local_host, local_port,
+                                           forward_host, forward_port);
+      acceptor.accept_connections();
+      ios.run();
+   }
+   catch(std::exception& e)
+   {
+      std::cerr << "Error: " << e.what() << std::endl;
+      return 1;
+   }
+
+   return 0;
+}
+
+/*
+ * [Note] On posix systems the tcp proxy server build command is as follows:
+ * c++ -pedantic -ansi -Wall -Werror -O3 -o tcpproxy_server tcpproxy_server.cpp -L/usr/lib -lstdc++ -lpthread -lboost_thread -lboost_system
+ */
