d8af9a29a17d39250b4212bb913ea91599c2c9fa
SamuelWenninger <wennis@rpi.edu>
2015-11-30 21:09:53 -0500

    Limited transfers and deposits to maximum digits possible for a long long

--- validate.h
+++ validate.h
@@ -5,8 +5,8 @@
 //Check if the given command is a valid bank command shell command
 bool IsValidBankCommand(std::string command) {                                       
     std::vector<boost::regex> Allowed =
-                    {boost::regex("^deposit\\s{1}(alice|bob|eve)\\s{1}\\d+$"), 
-                     boost::regex("^balance\\s{1}(alice|bob|eve)$")};
+                {boost::regex("^deposit\\s{1}(alice|bob|eve)\\s{1}\\d{1,19}$"), 
+                 boost::regex("^balance\\s{1}(alice|bob|eve)$")};
     for (auto &i: Allowed) {
         if (boost::regex_match(command, i)) {
             return true;
@@ -20,8 +20,8 @@ bool IsValidATMCommand(std::string command) {
     std::vector<boost::regex> Allowed =
                 {boost::regex("^login\\s{1}(alice|bob|eve)$"), 
                  boost::regex("^balance$"), 
-                 boost::regex("^withdraw\\s{1}\\d{1,20}$"), 
-                 boost::regex("^transfer\\s{1}\\d{1,20}\\s{1}(alice|bob|eve)$"), 
+                 boost::regex("^withdraw\\s{1}\\d{1,19}$"), 
+                 boost::regex("^transfer\\s{1}\\d{1,19}\\s{1}(alice|bob|eve)$"), 
                  boost::regex("^logout$")};
     for (auto &i: Allowed) {
         if (boost::regex_match(command, i)) {
