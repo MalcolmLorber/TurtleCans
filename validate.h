ed433979a50a8100bfeffdbf588ecd4e91cbc13d
SamuelWenninger <wennis@rpi.edu>
2015-12-02 05:47:26 -0500

    Only 18 long

--- validate.h
+++ validate.h
@@ -5,7 +5,7 @@
 //Check if the given command is a valid bank command shell command
 bool IsValidBankCommand(std::string command) {                                       
     std::vector<boost::regex> Allowed =
-                {boost::regex("^deposit\\s{1}(alice|bob|eve)\\s{1}\\d{1,19}$"), 
+                {boost::regex("^deposit\\s{1}(alice|bob|eve)\\s{1}\\d{1,18}$"), 
                  boost::regex("^balance\\s{1}(alice|bob|eve)$")};
     for (auto &i: Allowed) {
         if (boost::regex_match(command, i)) {
@@ -22,8 +22,8 @@ bool IsValidATMCommand(std::string command) {
                  boost::regex("^login\\s{1}(912343|396175|431253)$"), 
                  boost::regex("^\\d{1,6}$"), 
                  boost::regex("^balance$"), 
-                 boost::regex("^withdraw\\s{1}\\d{1,19}$"), 
-                 boost::regex("^transfer\\s{1}\\d{1,19}\\s{1}(alice|bob|eve)$"), 
+                 boost::regex("^withdraw\\s{1}\\d{1,18}$"), 
+                 boost::regex("^transfer\\s{1}\\d{1,18}\\s{1}(alice|bob|eve)$"), 
                  boost::regex("^logout$")};
     for (auto &i: Allowed) {
         if (boost::regex_match(command, i)) {
